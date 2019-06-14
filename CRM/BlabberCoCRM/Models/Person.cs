5051bb2fced89b6047df745cc12de816d4bd4838
Steve <crossley_steven@hotmail.com>
2019-06-14 16:49:17 +0100

    Added Person

--- CRM/BlabberCoCRM/Models/Person.cs
+++ CRM/BlabberCoCRM/Models/Person.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlabberCoCRM.Models
 {
-    public class Person
+    public abstract class Person
     {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public String PhoneNumber { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public String EmailAddress { get; set; }
     }
 }
