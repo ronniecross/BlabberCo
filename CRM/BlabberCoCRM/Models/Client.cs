b60c4a988dc30bfa14e276f7a5a3c1eda3c55545
Steve <crossley_steven@hotmail.com>
2019-06-17 11:09:46 +0100

    corrected type for Client.Name

--- CRM/BlabberCoCRM/Models/Client.cs
+++ CRM/BlabberCoCRM/Models/Client.cs
@@ -11,7 +11,7 @@ namespace BlabberCoCRM.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ID { get; set; }
-        public string nNme { get; set; }
+        public string Name { get; set; }
         [DataType(DataType.Date)]
         public DateTime SetupDate { get; set; }
         public string CostCenterCode { get; set; }
